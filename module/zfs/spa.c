bc93ace273bda1fccabc4349e15f89f0192a5bd8
Allan Jude <allan@klarasystems.com>
2021-03-23 22:12:25 +0000

    Remove old debug printfs that Linux doesn't like
    Signed-off-by: Allan Jude <allan@klarasystems.com>

--- module/zfs/spa.c
+++ module/zfs/spa.c
@@ -541,7 +541,6 @@ spa_prop_validate(spa_t *spa, nvlist_t *props)
 		case ZPOOL_PROP_INVAL:
 			if (zpool_prop_vdev(propname)) {
 				/* XXX: Allan: todo */
-printf("ALLAN: spa_prop_validate TODO: %s\n", propname);
 				break;
 			}
 			if (!zpool_prop_feature(propname)) {
@@ -787,7 +786,6 @@ spa_prop_set(spa_t *spa, nvlist_t *nvp)
 				VERIFY(nvpair_value_uint64(elem, &ver) == 0);
 			} else if (zpool_prop_vdev(nvpair_name(elem))) {
 				/* XXX: Allan: todo */
-printf("ALLAN: spa_prop_set TODO: %s\n", nvpair_name(elem));
 			} else {
 				ASSERT(zpool_prop_feature(nvpair_name(elem)));
 				ver = SPA_VERSION_FEATURES;
@@ -8641,7 +8639,6 @@ spa_sync_props(void *arg, dmu_tx_t *tx)
 		case ZPOOL_PROP_INVAL:
 			if (zpool_prop_vdev(nvpair_name(elem))) {
 				/* XXX: Allan: todo */
-printf("ALLAN: spa_sync_props TODO: %s\n", nvpair_name(elem));
 				break;
 			}
 			/*
